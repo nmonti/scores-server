46c30c6aff5dcc4442edeb44d18400fe2dcb9ca3
Nick <nickmax123@gmail.com>
2019-05-21 18:37:03 -0700

    remove graphql

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import 'babel-polyfill';
-import bodyParser from 'body-parser';
 import cors from 'cors';
 import express from 'express';
 import fetch from 'node-fetch';
-import { graphqlExpress, graphiqlExpress } from 'graphql-server-express';
-import { makeExecutableSchema } from 'graphql-tools';
 import path from 'path';
 //const mock = require('../mock/scoreboard.json');
 //const game = mock.games[0];
@@ -28,66 +25,70 @@ const getTodaysNBAScoreboard = async () => {
   return game.games[0];
 }
 
+const getTodaysMLBScoreboard = async () => {
+  const mlbTodayURI = 'http://statsapi.mlb.com/api/v1/schedule/games/?sportId=1';
+  const res = await fetch(mlbTodayURI);
+  const games = await res.json();
+  return games.dates[0].games;
+}
+
 // const because a function would be hoisted, and the imports would happen after
 export const init = async () => {
   try {
-
-    const game = await getTodaysNBAScoreboard();
-    console.log(game);
-    const typeDefs = [`
-      type Query {
-        nbaToday: NBAScoreboard
-      }
-
-      type NBAScoreboard {
-        _id: ID!
-        city: String!
-        homeTeam: String
-        homeScore: Int
-        awayTeam: String
-        awayScore: Int
-      }
-
-      schema {
-        query: Query
-      }
-    `];
-
-    const resolvers = {
-      Query: {
-        nbaToday: async () => {
-          return await Promise.resolve({
-            city: game.arena.city,
-            homeTeam: game.hTeam.triCode,
-            homeScore: game.hTeam.score,
-            awayTeam: game.vTeam.triCode,
-            awayScore: game.vTeam.score
-          });
+    const getNBAToday = async () => {
+      const nbaGames = await getTodaysNBAScoreboard();
+      const games = [nbaGames].flat().map(game => {
+        return {
+          city: game.arena.city,
+          homeTeam: game.hTeam.triCode,
+          homeScore: game.hTeam.score,
+          awayTeam: game.vTeam.triCode,
+          awayScore: game.vTeam.score
         }
-      }
-    };
+      });
 
-    const schema = makeExecutableSchema({
-      typeDefs,
-      resolvers
-    });
+      return games;
+    }
+
+    const getMLBToday = async () => {
+      const mlbGames = await getTodaysMLBScoreboard();
+      const games = mlbGames.map(game => {
+
+        return {
+          park: game.venue.name,
+          homeTeam: game.teams.home.team.name,
+          homeScore: game.teams.home.score || 0,
+          awayTeam: game.teams.away.team.name,
+          awayScore: game.teams.away.score || 0
+        }
+      });
+
+      return games;
+    }
+
+    //console.log(await getMLBToday());
 
     const app = express();
     const distPath = path.resolve(__dirname, '../dist');
 
     app.use(cors());
     app.use(express.static(distPath, {maxAge: 86400000}));
-    app.use('/graphql', bodyParser.json(), graphqlExpress({schema}));
-    app.use('/graphiql', graphiqlExpress({
-      endpointURL: '/graphql'
-    }));
 
     app.listen(PORT, () => {
       console.log(`http:/localhost:${PORT}`)
     });
 
+    app.get('/', (req, res) => {
+      res.send("try /mlb or /nba");
+    })
+
+    app.get('/mlb', async (req, res) => {
+      const scores = await getMLBToday();
+      res.send(scores);
+    })
+
   } catch (e) {
-    console.log(e)
+    console.log(e);
   }
 }
 
